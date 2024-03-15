1ba29bf92954522c71b1764e54b09b081d717467
Keita Ito <eguchi@eguchi.cc>
2024-03-15 15:28:35 +0000

    ヘッダーのデザインを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,27 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import Image from "next/image";
+import IconLogin from "@mui/icons-material/Login";
 
 export function Header() {
   return (
     <AppBar position="fixed" sx={{ bgcolor: "white" }}>
-      <Toolbar>
-        <Image
-          alt="UEC Review"
-          src="/logo.svg"
-          width={74}
-          height={10}
-          style={{
-            width: "auto",
-            height: "24px",
-          }}
-        />
-      </Toolbar>
+      <Container>
+        <Toolbar disableGutters>
+          <Image
+            alt="UEC Review"
+            src="/logo.svg"
+            width={74}
+            height={10}
+            style={{
+              width: "auto",
+              height: "24px",
+            }}
+          />
+          <Button sx={{ ml: "auto" }} startIcon={<IconLogin />}>
+            サインイン
+          </Button>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
