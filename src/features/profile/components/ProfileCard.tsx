3cd5895317b04a9c4c6c4076cc2637cb985fc91c
Keita Ito <eguchi@eguchi.cc>
2024-03-15 12:55:49 +0000

    アイコングループの作成

--- src/features/profile/components/ProfileCard.tsx
+++ src/features/profile/components/ProfileCard.tsx
@@ -1,8 +1,12 @@
-import { Paper, Typography } from "@mui/material";
+import { Avatar, Box, Paper, Typography } from "@mui/material";
 
 export function ProfileCard() {
   return (
     <Paper sx={{ p: 2 }}>
+      <Box display="flex" alignItems="center" gap={2}>
+        <Avatar />
+        <Box>名前</Box>
+      </Box>
       <Typography variant="h1">Hello</Typography>
     </Paper>
   );
