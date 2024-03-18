662b1d4363be0c6470f1afaf2d90159f063cb4a2
Keita Ito <eguchi@eguchi.cc>
2024-03-18 10:55:17 +0000

    順番がおかしい

--- src/features/review/getReviews.ts
+++ src/features/review/getReviews.ts
@@ -4,9 +4,9 @@ export async function getReviews() {
   const { data, error } = await supabase
     .from("reviews")
     .select("*, classes (name), users (name, avatar_url)")
-    .order("created_at", { ascending: false })
     .neq("id", Math.floor(Math.random() * 1000)) // キャッシュが残る
-    .limit(100);
+    .order("created_at", { ascending: false })
+    .limit(50);
   if (error) throw error;
   return { reviews: data };
 }
