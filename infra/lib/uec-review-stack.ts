d65cd5672fc55994cbed3932fc83deb6543f71df
Keita Ito <eguchi@eguchi.cc>
2024-08-20 00:29:39 +0900

    chore: lambdaの構成

--- infra/lib/uec-review-stack.ts
+++ infra/lib/uec-review-stack.ts
@@ -2,35 +2,37 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class UecReviewStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "MyVpc", {
-      subnetConfiguration: [
-        {
-          subnetType: ec2.SubnetType.PUBLIC,
-          name: "public",
-          cidrMask: 24,
-        },
-      ],
-      maxAzs: 1,
+    const natGatewayProvider = ec2.NatProvider.instanceV2({
+      instanceType: new ec2.InstanceType("t3.nano"),
+      defaultAllowedTraffic: ec2.NatTrafficDirection.OUTBOUND_ONLY,
     });
+
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 1,
+      natGatewayProvider: natGatewayProvider,
+    });
+
+    natGatewayProvider.securityGroup.addIngressRule(
+      ec2.Peer.ipv4(vpc.vpcCidrBlock),
+      ec2.Port.allTraffic()
+    );
 
     const securityGroup = new ec2.SecurityGroup(this, "MySecurityGroup", {
       vpc: vpc,
     });
-
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
 
     const instance = new ec2.Instance(this, "MyInstance", {
       vpc: vpc,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO
-      ),
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      instanceType: new ec2.InstanceType("t2.micro"),
       machineImage: ec2.MachineImage.genericLinux({
         "ap-northeast-1": "ami-0a0b7b240264a48d7",
       }),
@@ -38,8 +40,20 @@ export class UecReviewStack extends cdk.Stack {
       keyPair: ec2.KeyPair.fromKeyPairName(this, "MyKeyPair", "m1mac"),
     });
 
-    new cdk.CfnOutput(this, "publicIp", {
-      value: instance.instancePublicIp,
+    const func = new lambda.DockerImageFunction(this, "MyFunction", {
+      code: lambda.DockerImageCode.fromImageAsset("../app"),
+      architecture: lambda.Architecture.X86_64,
+      vpc: vpc,
+      environment: {
+        SERVER_IP: instance.instancePrivateIp,
+      },
+    });
+    const functionUrl = func.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: functionUrl.url,
     });
   }
 }
