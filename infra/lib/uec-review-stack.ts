e01bf48e972aeeba5997e63ec0a7246d89f8f2e6
Keita Ito <eguchi@eguchi.cc>
2024-08-19 19:42:23 +0900

    chore: ec2の構成

--- infra/lib/uec-review-stack.ts
+++ infra/lib/uec-review-stack.ts
@@ -1,16 +1,45 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class UecReviewStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: "public",
+          cidrMask: 24,
+        },
+      ],
+      maxAzs: 1,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'UecReviewQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const securityGroup = new ec2.SecurityGroup(this, "MySecurityGroup", {
+      vpc: vpc,
+    });
+
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
+
+    const instance = new ec2.Instance(this, "MyInstance", {
+      vpc: vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.genericLinux({
+        "ap-northeast-1": "ami-0a0b7b240264a48d7",
+      }),
+      securityGroup: securityGroup,
+      keyPair: ec2.KeyPair.fromKeyPairName(this, "MyKeyPair", "m1mac"),
+    });
+
+    new cdk.CfnOutput(this, "publicIp", {
+      value: instance.instancePublicIp,
+    });
   }
 }
